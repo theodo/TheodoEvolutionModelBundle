1af171b65a226c6ba9aec94a9f3e6e48f5425edf
Jonathan B <jonathanb@theodo.fr>
2012-10-03 17:21:04 +0200

    init bundle

---
+++ TheodoEvolutionLegacyModelBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Theodo\Evolution\LegacyModelBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class TheodoEvolutionLegacyModelBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}
